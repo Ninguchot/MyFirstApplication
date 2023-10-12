feed4d284f83b651ccb4d54b3ce7ecfbfebff8e1
ander <Anderson.almeida.21012@hotmail.com>
2023-10-12 19:30:58 -0300

    SetOnClickListener

--- app/src/main/java/com/example/da/myfirstapplication/MainActivity.kt
+++ app/src/main/java/com/example/da/myfirstapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.da.myfirstapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 
@@ -13,6 +14,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        campo1 = findViewById()
+        campo1 = findViewById(R.id.nome)
+        botao1 = findViewById(R.id.botao)
+        botao1.setOnClickListener(object : View.OnClickListener{
+
+        })
+
     }
 }
